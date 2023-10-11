9a54f7d3d02e28fa2529b45ee933d0ebc69e96a2
tienphudev <phuqtp@gmail.com>
2023-10-11 10:27:03 +0700

    :chi so BMI

--- CV cá nhân/ungdungmaytinh.js
+++ CV cá nhân/ungdungmaytinh.js
@@ -1,19 +1,22 @@
 let result;
 function sumWeb() {
-  let number1 = +document.getElementById("Number 1").value;
-  let number2 = +document.getElementById("Number 2").value;
+  let number1 = +document.getElementById("Number1").value;
+  let number2 = +document.getElementById("Number2").value;
   let tong = sum(number1, number2);
 
   document.getElementById("Result").innerHTML = tong;
 }
 
-function sum(a, b) {
-  return a + b;
+function sum(Number1, Number2) {
+  return Number1 + Number2;
 }
 
-function sub() {
-  let number1 = +document.getElementById("Number 1").value;
-  let number2 = +document.getElementById("Number 2").value;
-  let Tru = number1 - number2;
+function subWeb() {
+  let number1 = +document.getElementById("Number1").value;
+  let number2 = +document.getElementById("Number2").value;
+  let Tru = sub(number1, number2);
   document.getElementById("Result").innerHTML = Tru;
+}
+function sub(a, b) {
+  return a - b;
 }
